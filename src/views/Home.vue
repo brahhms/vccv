d0d55d9c7910e07076f6dddf52395feb71f68d3a
Efrain Orellana <abrahamatteo@gmail.com>
2022-01-28 11:55:35 -0600

    formato movil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,138 +12,147 @@
 
         <v-stepper-items>
           <v-stepper-content step="1">
-            <v-card class="mb-12" color="grey lighten-1" min-height="70vh">
-              <v-sheet min-height="70vh" rounded="lg" class="px-12 pt-8">
-                <v-form ref="form" v-model="valid" lazy-validation>
-                  <b>Vendedor</b>
+            <v-sheet min-height="80vh" rounded="lg" class="px-6 pt-8">
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <b>Vendedor</b>
 
-                  <v-combobox
-                    v-model="cv.vendedor"
-                    :items="personas"
-                    item-text="nombreCompleto"
-                    required
-                    :rules="nameRules"
-                    label="Nombre Completo"
-                    return-object
-                  >
-                  </v-combobox>
+                <v-combobox
+                  v-model="cv.vendedor"
+                  :items="personas"
+                  item-text="nombreCompleto"
+                  required
+                  :rules="nameRules"
+                  label="Nombre Completo"
+                  return-object
+                >
+                </v-combobox>
 
-                  <v-text-field
-                    v-model="cv.vendedor.dui"
-                    label="DUI"
-                    required
-                  ></v-text-field>
+                <v-text-field
+                  v-model="cv.vendedor.dui"
+                  label="DUI"
+                  required
+                ></v-text-field>
 
-                  <v-autocomplete
-                    v-model="cv.vendedor.domicilio"
-                    :items="municipios"
-                    item-text="nombre"
-                    required
-                    :rules="[(v) => !!v || 'Item is required']"
-                    label="Domicilio"
-                    return-object
-                  >
-                    <template v-slot:selection="data">
-                      {{ data.item.nombre }}
-                    </template>
-                    <template v-slot:item="data">
-                      {{ data.item.nombre }}
-                    </template>
-                  </v-autocomplete>
+                <v-autocomplete
+                  v-model="cv.vendedor.domicilio"
+                  :items="municipios"
+                  item-text="nombre"
+                  required
+                  :rules="[(v) => !!v || 'Item is required']"
+                  label="Domicilio"
+                  return-object
+                >
+                  <template v-slot:selection="data">
+                    {{ data.item.nombre }}
+                  </template>
+                  <template v-slot:item="data">
+                    {{ data.item.nombre }}
+                  </template>
+                </v-autocomplete>
 
-                  <v-text-field
-                    v-model="cv.vendedor.domicilio.departamento.nombre"
-                    label="Departamento"
-                    required
-                    disabled
-                  ></v-text-field>
-                </v-form>
-              </v-sheet>
-            </v-card>
+                <v-text-field
+                  v-model="cv.vendedor.domicilio.departamento.nombre"
+                  label="Departamento"
+                  required
+                  disabled
+                ></v-text-field>
+              </v-form>
+              <v-row class="mt-6">
+                <v-spacer></v-spacer>
 
-            <v-btn color="primary" @click="nextStep(1)"> Continue </v-btn>
-
-            <v-btn text> Cancel </v-btn>
+                <v-btn color="primary" @click="nextStep(1)"> Continuar </v-btn>
+              </v-row>
+            </v-sheet>
           </v-stepper-content>
 
           <v-stepper-content step="2">
-            <v-card class="mb-12" color="grey lighten-1" min-height="70vh">
-              <v-sheet min-height="70vh" rounded="lg" class="px-12 pt-8">
-                <v-form ref="form" v-model="valid" lazy-validation>
-                  <b>Comprador</b>
+            <v-sheet min-height="80vh" rounded="lg" class="px-6 pt-8">
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <b>Comprador</b>
 
-                  <v-combobox
-                    v-model="cv.comprador"
-                    :items="personas"
-                    item-text="nombreCompleto"
-                    required
-                    :rules="nameRules"
-                    label="Nombre Completo"
-                    return-object
-                  >
-                  </v-combobox>
+                <v-combobox
+                  v-model="cv.comprador"
+                  :items="personas"
+                  item-text="nombreCompleto"
+                  required
+                  :rules="nameRules"
+                  label="Nombre Completo"
+                  return-object
+                >
+                </v-combobox>
 
-                  <v-text-field
-                    v-model="cv.comprador.dui"
-                    label="DUI"
-                    required
-                  ></v-text-field>
+                <v-text-field
+                  v-model="cv.comprador.dui"
+                  label="DUI"
+                  required
+                ></v-text-field>
 
-                  <v-autocomplete
-                    v-model="cv.comprador.domicilio"
-                    :items="municipios"
-                    item-text="nombre"
-                    required
-                    :rules="[(v) => !!v || 'Item is required']"
-                    label="Domicilio"
-                    return-object
-                  >
-                    <template v-slot:selection="data">
-                      {{ data.item.nombre }}
-                    </template>
-                    <template v-slot:item="data">
-                      {{ data.item.nombre }}
-                    </template>
-                  </v-autocomplete>
+                <v-autocomplete
+                  v-model="cv.comprador.domicilio"
+                  :items="municipios"
+                  item-text="nombre"
+                  required
+                  :rules="[(v) => !!v || 'Item is required']"
+                  label="Domicilio"
+                  return-object
+                >
+                  <template v-slot:selection="data">
+                    {{ data.item.nombre }}
+                  </template>
+                  <template v-slot:item="data">
+                    {{ data.item.nombre }}
+                  </template>
+                </v-autocomplete>
 
-                  <v-text-field
-                    v-model="cv.comprador.domicilio.departamento.nombre"
-                    label="Departamento"
-                    required
-                    disabled
-                  ></v-text-field>
-                </v-form>
-              </v-sheet>
-            </v-card>
-
-            <v-btn color="primary" @click="nextStep(2)"> Continue </v-btn>
-
-            <v-btn text> Cancel </v-btn>
+                <v-text-field
+                  v-model="cv.comprador.domicilio.departamento.nombre"
+                  label="Departamento"
+                  required
+                  disabled
+                ></v-text-field>
+              </v-form>
+              <v-row class="mt-6">
+                <v-spacer></v-spacer>
+                <v-btn text @click="nextStep(3)"> Regresar </v-btn>
+                <v-btn color="primary" @click="nextStep(2)"> Continuar </v-btn>
+              </v-row>
+            </v-sheet>
           </v-stepper-content>
 
           <v-stepper-content step="3">
-            <v-card
-              class="mb-12 py-4"
-              color="grey lighten-1"
-              min-height="70vh"
-            >
+            <v-card class="py-2" min-height="80vh">
               <v-sheet
-                height="792"
-                width="612"
+                height="764"
+                width="590"
                 elevation="5"
-                class="px-4 py-4 contenedor"
-                style="text-align: justify"
-                color="white" 
+                class="px-3 contenedor"
+                color="white"
               >
+                <div class="encabezado">
+                  <v-row>
+                    <v-col cols="2">
+                      <v-img
+                        alt="Logo gobierno"
+                        contain
+                        src="../assets/logo.svg"
+                        transition="scale-transition"
+                        width="30"
+                      ></v-img>
+                    </v-col>
+                    <v-col>
+                      <b> ALCALDIA MUNICIPAL DE CANDELARIA DE LA FRONTERA </b>
+                      <b>Departamento de Santa Ana</b>
+                    </v-col>
+                  </v-row>
+                </div>
                 <div class="sello oficinaCentral">sello oficina central</div>
                 <div class="contenido">
                   <v-row>
-                    <v-col class="text-center">
-                      <b>CERTIFICADO DE CARTA DE VENTA</b>
+                    <v-col cols="12" class="text-center pb-1">
+                      <b class="f-11">CERTIFICADO DE CARTA DE VENTA</b>
                     </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col>
+
+                    <v-col class="pt-0">
                       Don:
                       <b>
                         {{ cv.vendedor.nombreCompleto }}. DUI:
@@ -187,11 +196,12 @@
                 <div class="sello">exento</div>
                 <div class="pie-pagina">fierro</div>
               </v-sheet>
+              <v-row class="mt-4 pr-8">
+                <v-spacer></v-spacer>
+                <v-btn text @click="nextStep(3)"> Regresar </v-btn>
+                <v-btn color="primary" @click="nextStep(3)"> OK </v-btn>
+              </v-row>
             </v-card>
-
-            <v-btn color="primary" @click="nextStep(3)"> Continue </v-btn>
-
-            <v-btn text> Cancel </v-btn>
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
@@ -309,22 +319,50 @@ export default {
 </script>
 
 <style scoped>
-
 .contenedor {
   display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-rows: 200px 200px 150px;
+  grid-template-columns: 1fr 4fr;
+  grid-template-rows: 50px 110px 110px 150px;
   margin: 0px auto;
   border-radius: 0px !important;
+  padding: 10px;
+  font-size: 15px;
 }
 
-.contenedor > div {
-  padding: 20px;
+.encabezado {
+  font-size: 15px;
+  font-family: "Times New Roman", Times, serif;
+}
+
+@media only screen and (max-width: 600px) {
+  .contenedor {
+    width: 340px !important;
+    height: 440px !important;
+    font-size: 8px;
+  }
+
+  .encabezado {
+    font-size: 8px !important;
+  }
+
+  .container {
+    padding: 0px !important;
+  }
 }
 
 .contenido {
-  grid-row: 1/3;
+  grid-row: 2/4;
   grid-column: 2 / 4;
+  padding: 0px 5px;
+  line-height: 1.5em;
+  text-align: justify;
+}
+
+.encabezado {
+  grid-row: 1;
+  grid-column: 1 / 4;
+  font-family: "Times New Roman", Times, serif;
+  padding-left: 14px;
 }
 
 .sello {
